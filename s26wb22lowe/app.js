ca2dce818db2a70fd33d0db7d87c24b09a45d14d
TC Lowe <s272122@nwmissouri.edu>
2026-04-01 20:29:12 -0500

    Search Results
    That isn't what I wanted to name it, but fine.

--- s26wb22lowe/app.js
+++ s26wb22lowe/app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/bombs', function(req, res) {
+  res.render('bombs', { title: 'Search Results bombs' });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
