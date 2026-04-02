58f248174a1edd96126fdd5d526d7a22e8be2544
TC Lowe <s272122@nwmissouri.edu>
2026-04-01 20:51:32 -0500

    New Page
    Created the new page and configured it.

--- s26wb22lowe/app.js
+++ s26wb22lowe/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/grid', gridRouter);
+app.use('/selector', pickRouter);
 app.get('/bombs', function(req, res) {
   res.render('bombs', { title: 'Search Results bombs' });
 });
